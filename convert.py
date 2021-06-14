4f38869917295118c0a5be42d68a3650a8bc5d50
bboc <bernhard.bockelbrink@gmail.com>
2021-06-14 17:37:40 +0200

    refactored converter

--- convert.py
+++ convert.py
@@ -58,6 +58,20 @@ class CirklonInstrument(object):
             if item not in [TRACK_VALUES, ROW_DEFS]:
                 instrument_data[item] = raw_instrument[item]
 
+        track_values, midi_cc_data, cc_defs_required = self._make_track_data(raw_instrument)
+
+        instrument_data[TRACK_VALUES] = track_values
+
+        # make CC_defs (ic necessary)
+        if cc_defs_required:
+            instrument_data[CC_DEFS] = self._make_cc_defs(midi_cc_data)
+
+        # TODO: process ROW_DEFS
+
+        return instrument_data
+
+    def _make_track_data(self, raw_instrument):
+
         track_values = {}
         midi_cc_data = []  # store cc data to create CC_defs if necessary
         cc_defs_required = False
@@ -91,34 +105,28 @@ class CirklonInstrument(object):
             # increment slot!
             slot = slot + 1
 
-        instrument_data[TRACK_VALUES] = track_values
+        return track_values, midi_cc_data, cc_defs_required
 
-        # make CC_defs (ic necessary)
-        if cc_defs_required:
-            # "CC_9" : { "label" : "EQLow", "min_val" : 0, "max_val" : 127, "start_val" : 64 },
-            cc_defs = {}
-            for item in midi_cc_data:
-                # [2, "SomeCt", 0, 127]
-                # generates in CC_defs: "CC_2" : { "label" : "SomeCt", "min_val" : 0, "max_val" : 127 },
-                # [3, "Switch1", 0, 4, 2]
-                # generates "CC_3" : { "label" : "Switch1", "min_val" : 0, "max_val" : 4, "start_val" : 2 }
-                cc_data = {
-                    'label': item[1]
-                }
-                if len(item) > 2:
-                    cc_data['min_val'] = item[2]
-                if len(item) > 3:
-                    cc_data['max_val'] = item[3]
-                if len(item) > 4:
-                    cc_data['start_val'] = item[4]
+    def _make_cc_defs(self, midi_cc_data):
+        # "CC_9" : { "label" : "EQLow", "min_val" : 0, "max_val" : 127, "start_val" : 64 },
+        cc_defs = {}
+        for item in midi_cc_data:
+            # [2, "SomeCt", 0, 127]
+            # generates in CC_defs: "CC_2" : { "label" : "SomeCt", "min_val" : 0, "max_val" : 127 },
+            # [3, "Switch1", 0, 4, 2]
+            # generates "CC_3" : { "label" : "Switch1", "min_val" : 0, "max_val" : 4, "start_val" : 2 }
+            cc_data = {
+                'label': item[1]
+            }
+            if len(item) > 2:
+                cc_data['min_val'] = item[2]
+            if len(item) > 3:
+                cc_data['max_val'] = item[3]
+            if len(item) > 4:
+                cc_data['start_val'] = item[4]
 
-                cc_defs["CC_%s" % item[0]] = cc_data
-
-            instrument_data[CC_DEFS] = cc_defs
-
-        # TODO: process ROW_DEFS
-
-        return instrument_data
+            cc_defs["CC_%s" % item[0]] = cc_data
+        return cc_defs
 
 
 if __name__ == "__main__":
